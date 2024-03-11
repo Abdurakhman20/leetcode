ebf5cb980b4cceae56f2457fc5e2d854de029bc2
Абдурахман Усманов <ausmanov706@gmail.com>
2024-03-12 00:21:37 +0300

    fix

---
+++ data_structures/stack.ts
@@ -0,0 +1,42 @@
+class Stack {
+  private count: number;
+  private storage: {[key: number]: number};
+  constructor() {
+    this.count = 0;
+    this.storage = {};
+  }
+
+  push(value: number) {
+    this.storage[this.count] = value;
+    this.count++;
+  }
+
+  pop() {
+    if(this.count === 0) return undefined;
+    this.count--;
+    let res = this.storage[this.count];
+    delete this.storage[this.count];
+
+    return res;
+  }
+
+  peek() {
+    return this.storage[this.count - 1];
+  }
+
+  size() {
+    return this.count;
+  }
+}
+
+const stack = new Stack();
+stack.push(1);
+stack.push(2);
+stack.push(3);
+stack.push(4);
+stack.push(5);
+
+
+console.log(stack.size());
+console.log(stack.pop());
+console.log(stack.size());
